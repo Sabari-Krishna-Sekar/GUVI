d4fd14acdb55babc940e771b7046c7fae162d70c
VEGETA05 <53682215+VEGETA05@users.noreply.github.com>
2019-08-04 11:36:03 +0530

    Update oddoreven.c

--- oddoreven.c
+++ oddoreven.c
@@ -5,7 +5,11 @@ void main()
  int a;
  printf("enter a value");
  scanf("%d",&a);
- if(a%2==0)
+ if(a<0)
+ {
+   printf("enter a valid number");
+ } 
+ else if(a%2==0)
  { 
   printf("the value is even");
  }
