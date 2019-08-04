9708e1f190630f2278545f0a3736a3bf8a678b20
VEGETA05 <53682215+VEGETA05@users.noreply.github.com>
2019-08-04 11:31:50 +0530

    Create oddoreven.c

---
+++ oddoreven.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int a;
+ printf("enter a value");
+ scanf("%d",&a);
+ if(a%2==0)
+ { 
+  printf("the value is even");
+ }
+ else
+ {
+  printf("the value is odd");
+ }
+} 
